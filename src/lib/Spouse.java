c737c978a141833866d57b16d05680ff21d341ed
zidan <zidansivafajar23@gmail.com>
2025-04-18 10:18:10 +0700

    Membuat class baru untuk variabel yang berpasangan)

---
+++ src/lib/Spouse.java
@@ -0,0 +1,9 @@
+public class Spouse {
+    private String name;
+    private String idNumber;
+
+    public Spouse(String name, String idNumber) {
+        this.name = name;
+        this.idNumber = idNumber;
+    }
+}
